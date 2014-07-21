57414c95780164b4be1b3cc95aad61cf93a163df
Alexis Chantreau <achantreau@hotmail.fr>
2014-07-21 11:53:32 +0100

    Update working_with_files.py

--- Python Village/working_with_files.py
+++ Python Village/working_with_files.py
@@ -1,6 +1,8 @@
 if __name__ == '__main__':
-    file = open("test.txt", "r")	
+    file = open("test.txt", "r")
+    
     for line_number, line in enumerate(file, start=1):
-	    if line_number % 2 == 0:
-		    print line,
+    	if line_number % 2 == 0:
+    	    print line,
+    
     file.close()
